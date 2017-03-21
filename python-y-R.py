84d50c656bd612fdd7c407a636c830d836ebea3e
Ana Diedrichs <ana.diedrichs@frm.utn.edu.ar>
2017-03-21 00:51:42 -0300

    archivos de pruebas

---
+++ python-y-R.py
@@ -0,0 +1,8 @@
+from numpy import *
+import scipy as sp
+from pandas import *
+from rpy2.robjects.packages import importr
+import rpy2.robjects as ro
+import pandas.rpy.common as com
+
+#https://sites.google.com/site/aslugsguidetopython/data-analysis/pandas/calling-r-from-python
